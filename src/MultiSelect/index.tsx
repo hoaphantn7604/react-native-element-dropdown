b10b87499e265284dd4533438b8f2d44dc046175
hoa.phan-van <hoa.phan-van@banvien.com.vn>
2021-09-22 17:22:25 +0700

    fix error position

--- src/MultiSelect/index.tsx
+++ src/MultiSelect/index.tsx
@@ -192,19 +192,20 @@ const MultiSelectComponent: MultiSelect = (props) => {
         isFull,
         w,
         top,
-        bottom
+        bottom,
+        left
       } = position
       if (w && top && bottom) {
         return <Modal transparent visible={visible} supportedOrientations={['landscape', 'portrait']}>
           <TouchableWithoutFeedback onPress={showOrClose}>
-            <View style={[{ width: W, height: H, alignItems: 'center' }, isFull && { backgroundColor: 'rgba(0,0,0,0.2)' }]}>
+            <View style={[{ width: W, height: H}, isFull && { backgroundColor: 'rgba(0,0,0,0.2)' }]}>
               <View style={{ height: top, width: w, justifyContent: 'flex-end' }}>
-                {bottom < maxHeight && <View style={[{ width: w }, styles.container, containerStyle, isFull ? { marginBottom: scale(20), width: W / 2, alignSelf: 'center' } : { maxHeight: maxHeight }]}>
+                {bottom < maxHeight && <View style={[{ width: w, left: left, }, styles.container, containerStyle, isFull ? { marginBottom: scale(20), width: W / 2, alignSelf: 'center' } : { maxHeight: maxHeight }]}>
                   {_renderListTop()}
                 </View>}
               </View>
               <View style={{ height: bottom, width: w }}>
-                {bottom > maxHeight && <View style={[{ width: w }, styles.container, containerStyle, isFull ? { marginBottom: scale(20), width: W / 2, alignSelf: 'center' } : { maxHeight: maxHeight }]}>
+                {bottom > maxHeight && <View style={[{ width: w, left: left, }, styles.container, containerStyle, isFull ? { marginBottom: scale(20), width: W / 2, alignSelf: 'center' } : { maxHeight: maxHeight }]}>
                   {_renderListBottom()}
                 </View>}
               </View>
@@ -225,12 +226,14 @@ const MultiSelectComponent: MultiSelect = (props) => {
         const w = px;
         const top = isFull ? scale(20) : py + fy + scale(2);
         const bottom = H - top;
+        const left = fx;
 
         setPosition({
           isFull,
           w,
           top,
-          bottom
+          bottom,
+          left
         });
       })
     }
