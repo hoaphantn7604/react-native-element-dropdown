4355154ae2a14dd05b245bbc1024818e9c14cf8d
Hoa Phan <hoaphantn7604@gmail.com>
2022-06-12 21:46:22 +0700

    fix: position

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -472,7 +472,7 @@ const DropdownComponent = React.forwardRef<any, DropdownProps>(
           const styleVertical: ViewStyle = { left: left, maxHeight: maxHeight };
           const isTopPosition =
             dropdownPosition === 'auto'
-              ? bottom < maxHeight - 50
+              ? bottom < (isIOS ? 200 : 300)
               : dropdownPosition === 'top'
               ? true
               : false;
