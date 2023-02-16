253e0afa3c0aac52b956e1b09f336241cc2bbb8b
hoaphantn7604@gmail.com <hoaphantn7604@gmail.com>
2023-02-16 15:23:58 +0700

    chore: export IMultiSelectRef

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@ import Dropdown from './components/Dropdown';
 import MultiSelect from './components/MultiSelect';
 import SelectCountry from './components/SelectCountry';
 import { IDropdownRef } from './components/Dropdown/model';
+import { IMultiSelectRef } from './components/MultiSelect/model';
 
-export { Dropdown, MultiSelect, SelectCountry, IDropdownRef };
+export { Dropdown, MultiSelect, SelectCountry, IDropdownRef, IMultiSelectRef };
